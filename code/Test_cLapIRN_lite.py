52287d801905862f5ecdccf3af6d8ce2992555f0
Tijmen-de-Haas <Tijmendehaas@hotmail.nl>
2023-05-14 16:47:43 +0200

    Update Test_cLapIRN_lite.py

--- code/Test_cLapIRN_lite.py
+++ code/Test_cLapIRN_lite.py
@@ -1,10 +1,11 @@
 import os
 from argparse import ArgumentParser
-
+import glob
+import torch.utils.data as Data
 import numpy as np
 import torch
 
-from Functions import generate_grid_unit, save_img, save_flow, transform_unit_flow_to_flow, load_4D_with_header, imgnorm
+from Functions import generate_grid_unit, Dataset_epoch,  save_img, save_flow, transform_unit_flow_to_flow, load_4D_with_header, imgnorm
 from miccai2021_model_lite import Miccai2021_LDR_conditional_laplacian_unit_disp_add_lvl1, \
     Miccai2021_LDR_conditional_laplacian_unit_disp_add_lvl2, Miccai2021_LDR_conditional_laplacian_unit_disp_add_lvl3, \
     SpatialTransform_unit, SpatialTransformNearest_unit
@@ -12,28 +13,33 @@ from miccai2021_model_lite import Miccai2021_LDR_conditional_laplacian_unit_disp
 
 parser = ArgumentParser()
 parser.add_argument("--modelpath", type=str,
-                    dest="modelpath", default='../Model/Lite_nlst_tryout.pth',
+                    dest="modelpath", default='D:/ismi_data/Model/Stage/Lite_nlst_tryoutstagelvl3_600.pth',
                     help="Trained model path")
 parser.add_argument("--savepath", type=str,
                     dest="savepath", default='../Result',
                     help="path for saving images")
 parser.add_argument("--start_channel", type=int,
-                    dest="start_channel", default=2,
+                    dest="start_channel", default=4,
                     help="number of start channels")
 parser.add_argument("--fixed", type=str,
-                    dest="fixed", default='../Data/image_A_fullsize.nii.gz',
+                    dest="fixed", default='D:/ismi_data/NLST/NLST_testdata/imagesTs/NLST_0111_0000.nii.gz',
                     help="fixed image")
 parser.add_argument("--moving", type=str,
-                    dest="moving", default='../Data/image_B_fullsize.nii.gz',
+                    dest="moving", default='D:/ismi_data/NLST/NLST_testdata/imagesTs/NLST_0111_0001.nii.gz',
                     help="moving image")
 parser.add_argument("--reg_input", type=float,
                     dest="reg_input", default=0.1,
                     help="Normalized smoothness regularization (within [0,1])")
+parser.add_argument("--datapath", type=str,
+                    dest="datapath",
+                    default='D:/ismi_data/NLST',
+                    help="data path for test images")
 opt = parser.parse_args()
 
 savepath = opt.savepath
 fixed_path = opt.fixed
 moving_path = opt.moving
+
 if not os.path.isdir(savepath):
     os.mkdir(savepath)
 
@@ -50,7 +56,7 @@ def test():
                                                                          range_flow=range_flow, model_lvl1=model_lvl1).cuda()
 
     model = Miccai2021_LDR_conditional_laplacian_unit_disp_add_lvl3(2, 3, start_channel, is_train=False, imgshape=imgshape,
-                                                                    range_flow=range_flow, model_lvl2=model_lvl2).cuda()
+                                                                   range_flow=range_flow, model_lvl2=model_lvl2).cuda()
 
 
     transform = SpatialTransform_unit().cuda()
@@ -66,27 +72,29 @@ def test():
     use_cuda = True
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    fixed_img, header, affine = load_4D_with_header(fixed_path)
-    moving_img, _, _ = load_4D_with_header(moving_path)
+    names = sorted(glob.glob(opt.datapath +'/NLST_testdata/imagesTs'+ '/*.nii.gz'))
 
-    fixed_img, moving_img = imgnorm(fixed_img), imgnorm(moving_img)
-    fixed_img = torch.from_numpy(fixed_img).float().to(device).unsqueeze(dim=0)
-    moving_img = torch.from_numpy(moving_img).float().to(device).unsqueeze(dim=0)
-
+    test_generator = Data.DataLoader(Dataset_epoch(names, norm=True), batch_size=1,
+                                         shuffle=True, num_workers=2)
+    
     with torch.no_grad():
-        reg_code = torch.tensor([reg_input], dtype=fixed_img.dtype, device=fixed_img.device).unsqueeze(dim=0)
+    
+        for i, (X, Y) in enumerate(test_generator):
+            X = X.cuda().float()
+            Y = Y.cuda().float()
+            reg_code = torch.rand(1, dtype=X.dtype, device=X.device).unsqueeze(dim=0)
 
-        F_X_Y = model(moving_img, fixed_img, reg_code)
+            F_X_Y = model(X, Y, reg_code)
+            X_Y = transform(Y, F_X_Y.permute(0, 2, 3, 4, 1), grid).data.cpu().numpy()[0, 0, :, :, :]
 
-        X_Y = transform(moving_img, F_X_Y.permute(0, 2, 3, 4, 1), grid).data.cpu().numpy()[0, 0, :, :, :]
+            F_X_Y_cpu = F_X_Y.data.cpu().numpy()[0, :, :, :, :].transpose(1, 2, 3, 0)
+            F_X_Y_cpu = transform_unit_flow_to_flow(F_X_Y_cpu)
+            subj = i + 111
 
-        F_X_Y_cpu = F_X_Y.data.cpu().numpy()[0, :, :, :, :].transpose(1, 2, 3, 0)
-        F_X_Y_cpu = transform_unit_flow_to_flow(F_X_Y_cpu)
+            #save_flow(F_X_Y_cpu, savepath + '/warpped_flow_full_' + 'reg' + str(subj) + str(reg_input) + '.nii.gz', header=header, affine=affine)
+            save_img(X_Y, savepath + '/warpped_moving_full_' + 'reg' + str(subj) + str(reg_input) + '.nii.gz', header=header, affine=affine)
 
-        save_flow(F_X_Y_cpu, savepath + '/warpped_flow_full_' + 'reg' + str(reg_input) + '.nii.gz', header=header, affine=affine)
-        save_img(X_Y, savepath + '/warpped_moving_full_' + 'reg' + str(reg_input) + '.nii.gz', header=header, affine=affine)
-
-    print("Result saved to :", savepath)
+    print("Results saved to :", savepath)
 
 
 if __name__ == '__main__':
